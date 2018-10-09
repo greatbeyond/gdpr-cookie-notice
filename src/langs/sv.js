e7e51734b1b054a4a49d840b3d236c08371899da
Björn Nordqvist <bjorn@nordqvist.io>
2018-10-09 16:16:49 +0200

    Swedish translation

---
+++ src/langs/sv.js
@@ -0,0 +1,17 @@
+//Add strings
+gdprCookieNoticeLocales.sv = {
+  description: "Vi använder cookies för att ge dig en bättre webbupplevelse och för att mäta hur webbplatsen används. Läs mer genom att klicka på ”Inställningar”.",
+  settings: "Inställningar",
+  accept: "Godkänn",
+  statement: "Läs våra integritetspolicy",
+  save: "Spara",
+  always_on: "Alltid på",
+  cookie_essential_title: "Grundläggande",
+  cookie_essential_desc: "Hjälper till att göra webbplatsen användbar genom att möjliggöra grundläggande funktioner såsom sidnavigering och säker åtkomst till webbplatsen. Webbplatsen kan inte fungera utan dessa cookies.",
+  cookie_performance_title: "Prestanda",
+  cookie_performance_desc: "Används för att förbättra prestanda och funktionalitet på webbplatsen.",
+  cookie_analytics_title: "Statistik",
+  cookie_analytics_desc: "Används för att hjälpa oss att mäta hur många som besökt samt hur användare har interagerat med webbplatsen. Statistiken loggas med maskerade IP-nummer som inte går att spåra till dig som individ.",
+  cookie_marketing_title: "Marknadsföring",
+  cookie_marketing_desc: "Används för att göra extern annonsering mer relevanta för dig. Avsikten är att visa annonser som är relevanta och engagerade för dig som användare och därmed mer värdefulla för utgivare och tredjepartsannonsörer. Sparas endast vid ditt aktiva godkännande."
+};
