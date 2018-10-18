a1169bda209ebe81dca24c7e8cff539730e5491f
Björn Nordqvist <bjorn@nordqvist.io>
2018-10-18 15:59:50 +0200

    Tracking code name

--- gdpr-cookie-notice.php
+++ gdpr-cookie-notice.php
@@ -177,14 +177,13 @@ class GDPRCookieNotice
                     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                    })(window,document,'script','https://www.google-analytics.com/analytics.js','__gaTracker');
 
-                    ga('create', '<?= $ga_id ?>', 'auto');
+                    __gaTracker('create', '<?= $ga_id ?>', 'auto');
 
 
                 <?php endif; ?>
                 document.addEventListener('gdprCookiesEvent', function (e) {
-                    console.log(e.detail);
                     <?php if ($px_id) : ?>
                         if (e.detail.marketing) {
                             !function(f,b,e,v,n,t,s)
@@ -200,7 +199,7 @@ class GDPRCookieNotice
                         }
                     <?php endif; ?>
                     <?php if ($ga_id) : ?>
-                        ga('send', 'pageview', {'anonymizeIp': !e.detail.analytics});
+                        __gaTracker('send', 'pageview', {'anonymizeIp': !e.detail.analytics});
                     <?php endif; ?>
                 });
             </script>
